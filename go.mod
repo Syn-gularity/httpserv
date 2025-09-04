baa64e97fa5d15acf281b4db3bc5e6a5baa240c8
Syn-gularity <jan_hendrik_ernst@hotmail.com>
2025-09-04 19:56:22 +0200

    routing

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Syn-gularity/httpserv
+
+go 1.24.4
--- main.go
+++ main.go
@@ -1,26 +1,58 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
 	"net/http"
+	"log"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricMaker(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	ret := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
+	w.Write([]byte(ret))
+}
+
+func (cfg *apiConfig) metricReset(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0) 
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("reset"))
+}
+
+func health(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
 func main() {
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 	mux := http.NewServeMux()
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+	
+	mux.HandleFunc("/metrics", apiCfg.metricMaker)
+	mux.HandleFunc("/reset", apiCfg.metricReset)
+	mux.HandleFunc("/healthz", health)
+
 	var srv http.Server
 	srv.Handler = mux
 	srv.Addr = ":8080"
-	srv.ListenAndServe()
 
-
-	/*mux.Handle("/api/", apiHandler{})
-	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		// The "/" pattern matches everything, so we need to check
-		// that we're at the root here.
-		if req.URL.Path != "/" {
-			http.NotFound(w, req)
-			return
-		}
-		fmt.Fprintf(w, "Welcome to the home page!")
-	})*/
+	fmt.Println("Starting Server")
+	log.Fatal(srv.ListenAndServe())
 }
